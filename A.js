143bd5d769aadbb52b9fa5f1f8d702b9aa884fdd
Madhan Mohan Manoharan <madhanmohan.manoharan@wegalvanize.com>
2021-05-29 22:17:28 +0530

    Update A and B js PR 3676

--- A.js
+++ A.js
@@ -3,3 +3,7 @@ console.log('Hello World from A.js - initial commit');
 
 //Update changes for preprod - Prod
 console.log('Hello World from A.js - Merge commit');
+
+//after reverting master merged PR
+//Update changes for preprod - Prod
+console.log('Hello World from A.js - Merge 2 commit');
